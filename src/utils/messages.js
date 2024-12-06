f099620da8a1aeb370a0296a29f42cd4991b7899
Kimi Maulana <kimi93.com@gamil.com>
2024-12-06 23:18:02 +0700

    added new progress

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,19 +1,20 @@
-const generateMessage = (username, text) =>{ 
-    return { 
-        username, 
-        text, 
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
         createdAt: new Date().getTime()
-    } 
-} 
- 
-const generateLocationMessage = (username, url) => { 
-    return { 
-        username, 
-        url, 
-        createdAt: new Date().getTime() 
-    } 
-} 
-module.exports = { 
-    generateMessage, 
-    generateLocationMessage 
-} 
+    };
+};
+
+const generateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    };
+};
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+};
